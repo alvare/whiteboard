e546cb502cd7c27badaea4d61bc2dc4f5055e207
clrnd <welcometothechango@gmail.com>
2018-01-31 11:23:29 -0300

    init with shader

---
+++ src/main.cpp
@@ -0,0 +1,89 @@
+#include <algorithm>
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <SFML/Graphics.hpp>
+
+std::string vertex_shader = R"glsl(
+#version 120;
+#extension GL_EXT_geometry_shader4 : enable
+
+int i;
+vec4 vertex;
+
+void main() {
+    // transform the vertex position
+    //gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+    for(i = 0; i < gl_VerticesIn; i++) {
+      gl_Position = gl_ModelViewProjectionMatrix * gl_PositionIn[i];
+      EmitVertex();
+    }
+
+    // transform the texture coordinates
+    //gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;
+
+    // forward the vertex color
+    gl_FrontColor = gl_Color;
+}
+)glsl";
+
+int main() {
+    using PointVec = std::vector<sf::Vertex>;
+
+    // create the window
+    sf::ContextSettings settings;
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window", sf::Style::Default, settings);
+    window.setVerticalSyncEnabled(true);
+    auto l = window.getSettings();
+    std::cout << l.majorVersion << ',' << l.minorVersion << std::endl;
+    //sf::View view = window.getView();
+    //view.zoom(2);
+    //window.setView(view);
+
+    if (!sf::Shader::isGeometryAvailable()) {
+        return 2;
+    }
+
+    sf::Shader shader;
+    if (!shader.loadFromMemory(vertex_shader, sf::Shader::Geometry)) {
+        return 1;
+    }
+
+    // empty vec of points to draw
+    std::unique_ptr<PointVec> points = std::make_unique<PointVec>(1, sf::Vertex());
+
+    // mouse position
+    sf::Vector2i prev_pos;
+
+    // run the program as long as the window is open
+    while (window.isOpen()) {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            sf::Vector2i pos = sf::Mouse::getPosition(window);
+            if (pos != prev_pos) {
+                points->push_back(sf::Vertex(sf::Vector2f(pos)));
+                prev_pos = pos;
+                std::cout << points->size() << '\n';
+            }
+        }
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything
+        window.draw(points->data(), points->size(), sf::LineStrip, &shader);
+
+        // end the current frame
+        window.display();
+    }
+
+    return 0;
+}
